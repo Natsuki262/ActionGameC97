e5fc66dd717ba60b0e441ddb84d225a774f3a256
NatsukiFGL <hiro.58.fgl.2525.952@icloud.com>
2019-08-25 00:37:57 +0900

    ＜add＞MoveScriptを追加しました。

---
+++ Assets/Move.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public Rigidbody rb;
+    [SerializeField]
+    private float Speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal") * Speed;
+        float z = Input.GetAxis("Vertical") * Speed;
+        rb.AddForce(x, 0, z);
+    }
+}
