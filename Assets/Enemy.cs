950a76df482a1a5f158c075f80fac120d16e65d7
yuuki_fujii <oneouts125@gmail.com>
2019-08-26 22:04:12 +0900

    敵にHPを設定。 今はAキーを4回押すと消える

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    int m_hp = 10;
 
     // Start is called before the first frame update
     void Start()
@@ -14,6 +15,18 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            Damage(3);
+        }
     }
     
+    void Damage(int damage)
+    {
+        m_hp -= damage;
+        if(m_hp <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
 }
