1f207143ecaf6e75bf0b7f8efa58fedbec6f1538
yuuki_fujii <oneouts125@gmail.com>
2019-08-28 22:02:46 +0900

    敵が自動でスポーンするようになった。 敵を全員倒したら数秒開けて敵がランダムな位置にスポーンする。

--- Assets/EnemySpawner.cs
+++ Assets/EnemySpawner.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
+    
+    public GameObject enemy;
+    public int enemyCount;
+    public float enemySpawnInterval;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine("Spawn");
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    IEnumerator Spawn()
+    {
+        while (true)
+        {
+            if (transform.childCount == 0)
+            {
+                yield return new WaitForSeconds(enemySpawnInterval);
+                for (int i = 0; i < enemyCount; i++)
+                {
+                    Instantiate(enemy, transform);
+                    float range = 10.0f;
+                    Vector3 position = new Vector3();
+                    position.x = Random.Range(-range, range);
+                    position.y = Random.Range(-range, range);
+                    position.z = Random.Range(-range, range);
+                    enemy.transform.position = position;
+                }
+            }
+            yield return null;
+        }
     }
 }
