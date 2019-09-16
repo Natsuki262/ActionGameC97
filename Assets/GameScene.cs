16150a5ade86fe9504dcd4eb7b1f5f1ee8b17660
yuuki_fujii <oneouts125@gmail.com>
2019-09-16 16:55:33 +0900

    ゲームシーンから時間切れになるとタイトルシーンに戻るようにした

--- Assets/GameScene.cs
+++ Assets/GameScene.cs
@@ -1,18 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameScene : MonoBehaviour
 {
+    GameTimer m_gameTimer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_gameTimer = GetComponent<GameTimer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(m_gameTimer.m_isTimeUp)
+        {
+            SceneManager.LoadScene("TitleScene");
+        }
     }
 }
