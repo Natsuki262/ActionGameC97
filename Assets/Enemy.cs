bcafa42809a4c1ccddd9ab2171e1f562b700a8a9
yuuki_fujii <oneouts125@gmail.com>
2019-09-21 02:11:06 +0900

    エネミーに剛体をつけてプレイヤーに向かって移動するようになった 回転もできた

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     int m_hp = 10;
-
+    public float speed = 5.0f;
     public GameObject m_popItem;
-
+    Rigidbody rigidbody;
+    GameObject player;
+    public float m_speed = 0.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody>();
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
@@ -21,9 +24,24 @@ public class Enemy : MonoBehaviour
         {
             Damage(3);
         }
+        Move();
     }
-    
-    void Damage(int damage)
+
+    void Move()
+    {
+        Vector3 direction = player.transform.position - transform.position;
+        direction.y = 0.0f;
+        direction.Normalize();
+        transform.rotation = Quaternion.LookRotation(direction);
+        Debug.Log(transform.rotation);
+        float moveY = rigidbody.velocity.y;
+        direction *= m_speed;
+        direction.y = moveY;
+        rigidbody.velocity = direction;
+
+    }
+
+    public void Damage(int damage)
     {
         m_hp -= damage;
         if(m_hp <= 0)
