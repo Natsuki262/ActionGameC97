50ddf321fb59166de6f11fd90652c91639058f79
NatsukiFGL <hiro.58.fgl.2525.952@icloud.com>
2019-09-26 21:20:41 +0900

    [fixed]回転処理を修正しました。

--- Assets/Move.cs
+++ Assets/Move.cs
@@ -15,11 +15,15 @@ public class Move : MonoBehaviour
     [SerializeField]
     private float JunpSpeed;
 
+    public GameObject targetObject;
+
     // Start is called before the first frame update
     void Start()
     {
         Player_pos = GetComponent<Transform>().position;//プレイヤーポジション取得
         rb = GetComponent<Rigidbody>();
+       
+
     }
 
     // Update is called once per frame
@@ -29,22 +33,41 @@ public class Move : MonoBehaviour
     }
     private void FixedUpdate()
     {
+            //Debug.Log(y);
         x = Input.GetAxis("Horizontal") * Speed;
         z = Input.GetAxis("Vertical") * Speed;
+        //Debug.Log(y);
         rb.AddForce(x, 0, z);
         //rb.velocity = Vector3.zero;
         //rb.angularVelocity = Vector3.zero;
 
-        /*Vector3 diff = transform.position - Player_pos;
-        Player_pos = transform.position;
-
+        //this.transform.LookAt(targetObject.transform);
+        
+        Vector3 diff = transform.position - Player_pos;
+        diff.y = 0;
+        //Player_pos = transform.position;
+        //Debug.Log(diff);
+            Debug.Log(diff+"a");
         if (diff.magnitude > 0.01f)//回転処理
         {
+            Debug.Log(diff+"b");
+
+
             transform.rotation = Quaternion.LookRotation(diff);
-        }*/
-        
+            //transform.rotationy= 0;
+            Debug.Log(diff+"c");
+
+
+
+            /*Debug.Log(x);
+            Debug.Log(z);
+           Debug.Log(transform.rotation);*/
+        }
+        Debug.Log(diff+"d");
+
 
         Player_pos = transform.position;
+        Debug.Log(diff+"e");
 
         Ray ray = new Ray(transform.position, Vector3.down);
         {
@@ -54,27 +77,27 @@ public class Move : MonoBehaviour
             {
                 // Debug.DrawRay(transform.position, Vector3.down , Color.green);
                 //Debug.DrawRay(transform.position, Vector3.down);
-                Debug.Log(hit.collider);
-              
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                //Debug.Log("エレガント");
-                rb.AddForce(transform.up * JunpSpeed);
-                Debug.Log(JunpSpeed);
-            }
-        
+                //Debug.Log(hit.collider);
+
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    //Debug.Log("エレガント");
+                    rb.AddForce(transform.up * JunpSpeed);
+                    // Debug.Log(JunpSpeed);
+                }
+
             }
 
         }
     }
-   /* void OnCollsionEnter(Collision collision)
-    {
-        if (collision.gameObject.tag == "Ground")
-        {
-            Debug.Log("Hit");
-            Debug.Log(collision.gameObject.name);
-        }
-    }*/
+    /* void OnCollsionEnter(Collision collision)
+     {
+         if (collision.gameObject.tag == "Ground")
+         {
+             Debug.Log("Hit");
+             Debug.Log(collision.gameObject.name);
+         }
+     }*/
 }
 
 
